f6fa7013a4f827aed70990fa501fa9855000a5e5
Yuratama1 <yuratama661@gmail.com>
2026-07-27 10:34:24 +0700

    menyelesaikan code

--- controller/userController.js
+++ controller/userController.js
@@ -56,7 +56,7 @@ async function login(req, res) {
         if(!user){
             return res.status(400).json({
                 message: 'Email atau password salah'
-            })
+            });
         }
 
         const isMatch = await bcrypt.compare(password, user.password);
@@ -65,5 +65,31 @@ async function login(req, res) {
                 message: 'Email atau password salah'
             });
         }
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: process.env.JWT_EXPIRES_IN
+            }
+        );
+
+        return res.status(200).json({
+            message: 'Login berhasil',
+            token
+        });
+
+    }catch (error){
+        return res.status(500).json({
+            message: error.message
+        });
     }
 }
+
+module.exports = {
+    register,
+    login
+};
